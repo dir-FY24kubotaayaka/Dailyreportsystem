c4b86c08ac23f1c837df835faea80d10cecf724d
dir-FY24ayakakubota <ayaka.kubota.z489@dirbato.co.jp>
2024-05-02 16:02:51 +0900

    コミット５8

--- daily_report_system/CREATE DATABASE daily_report_system DEFAULT CHARACTER SET utf8;.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE daily_report_system DEFAULT CHARACTER SET utf8;
-
-CREATE USER 'repuser'@'localhost' IDENTIFIED BY 'reppass';
-
-GRANT ALL PRIVILEGES ON daily_report_system.* to 'repuser'@'localhost';
